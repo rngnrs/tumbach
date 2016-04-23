e8a54da9a6ea59a29cfb65f12cd3ef1d35cf353c
rngnrs <rngnrs@yandex.ru>
2016-04-23 22:14:53 +0700

    Ololord updates

--- public/js/board.js
+++ public/js/board.js
@@ -1730,12 +1730,17 @@ lord.fileSelected = function(div) {
 
 lord.attachFileByLink = function(a) {
     var div = a.parentNode;
-    var url = prompt(lord.text("linkLabelText"), div.fileUrl);
-    if (null === url)
-        return;
-    lord.clearFileInput(div);
-    div.fileUrl = url;
-    lord.fileAddedCommon(div);
+    lord.prompt({
+        title: "linkLabelText",
+        value: div.fileUrl,
+        style: { minWidth: "350px" }
+    }).then(function(result) {
+        if (!result.accepted || !result.value)
+            return;
+        lord.clearFileInput(div);
+        div.fileUrl = result.value;
+        lord.fileAddedCommon(div);
+    }).catch(lord.handleError);
 };
 
 lord.setDrawingBackgroundColor = function(btn, color) {
@@ -2220,7 +2225,11 @@ lord.showUserIp = function(a) {
         boardName: boardName,
         postNumber: postNumber
     }).then(function(result) {
-        prompt("IP:", result.ipv4 || result.ip);
+        return lord.prompt({
+            title: "IP:",
+            value: result.ipv4 || result.ip,
+            readOnly: true
+        });
     }).catch(lord.handleError);
 };
 
